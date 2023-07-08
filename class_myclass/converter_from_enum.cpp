27c3587cba5bc497df2a90a97adf7fce6e861007
ArthurNigoyan <arthur.goyan@gmail.com>
2023-07-08 13:56:29 +0400

    update the existing file

--- class_myclass/converter_from_enum.cpp
+++ class_myclass/converter_from_enum.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
-enum week {
+
+
+class MLASS
+{
+	public:
+		enum week {
 		Monday = 1, 
 		Tuesday = 2, 
 		Wednesday = 3, 
@@ -9,46 +14,17 @@ enum week {
 		Saturday = 6, 
 		Sunday = 7
 		};
-
-class MLASS
-{
-	public:
-	
 		MLASS()
 		{
 			
 		};
 		~MLASS()
 		{};
-		int return_number_of_day(week obj)
+		week return_number_of_day(week obj)
 		{
-			switch(obj)
-			{
-				case Monday:
-					return 1;
-					break;
-				case Tuesday:
-					return 2;
-					break;
-				case Wednesday:
-					return 3;
-					break;
-				case Thursday:
-					return 4;
-					break;
-				case Friday:
-					return 5;
-					break;
-				case Saturday:
-					return 6;
-					break;
-				case Sunday:
-					return 7;
-					break;
-			}
-			return week::Monday;
+			return obj;
 		};
-		std::string return_name_of_day(int number_of_day)
+		std::string convert_To_enum(int number_of_day)
 		{
 			std::string s1;
 			switch(number_of_day)
@@ -83,7 +59,11 @@ class MLASS
 int main ()
 {
 	MLASS day;
-	std::cout<<day.return_number_of_day(Sunday)<<std::endl;
-	std::cout<<day.return_name_of_day(5)<<std::endl;
+	MLASS::week a = day.week::Sunday;
+	std::cout<<day.return_number_of_day(a)<<std::endl;
+	MLASS::week b = day.week::Wednesday;
+	std::cout<<a<<std::endl;
+	
+	std::cout<<day.convert_To_enum(b)<<std::endl;
 	return 0;
 }
