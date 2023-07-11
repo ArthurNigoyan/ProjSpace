9a3cb0260d92c2ce35c44c5826e82131836ebb90
ArthurNigoyan <arthur.goyan@gmail.com>
2023-07-12 01:03:03 +0400

    Move constructor and move operator assignment

---
+++ move_ctr_oper/animal.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+class ANIMAL
+{
+	public:
+		int* m_ptr;
+		int m_size;
+	private:
+		int m_weight;
+		int m_sum_of_weights;
+	public:
+		
+		ANIMAL(int animal_weight, int size) : m_weight{ animal_weight }, m_size {size}
+		{
+			m_sum_of_weights=m_weight;
+			m_ptr = new int [m_size];
+			for (int i = 0; i< m_size;i++)
+			{
+				m_ptr[i] = i;
+			}
+			std::cout<<"ANIMAL constructor"<<std::endl;
+		}
+		ANIMAL(ANIMAL &obj)
+		{
+			this->m_weight=obj.m_weight;
+			this->m_sum_of_weights=obj.m_sum_of_weights;
+			this->m_size=obj.m_size;
+			this->m_ptr=new int[this->m_size];
+			for (int i =0; i<obj.m_size;i++)
+			{
+				this->m_ptr[i]=obj.m_ptr[i];
+			}
+			std::cout<<"ANIMAL copy constructor"<<std::endl;
+		}
+		ANIMAL& operator=(ANIMAL& obj)
+		{
+			if (this==&obj)
+			{
+				return *this;
+			}
+			this->m_weight=obj.m_weight;
+			this->m_sum_of_weights=obj.m_sum_of_weights;
+			this->m_size=obj.m_size;
+			this->m_ptr=new int[this->m_size];
+			for (int i = 0; i< obj.m_size;i++)
+			{
+				this->m_ptr[i] = obj.m_ptr[i];
+			}
+			std::cout<<"ANIMAL operator assignment"<<std::endl;
+			return *this;
+		}
+		ANIMAL(ANIMAL&& obj)
+		{
+			this->m_weight=obj.m_weight;
+			this->m_sum_of_weights=obj.m_sum_of_weights;
+			this->m_size=obj.m_size;
+			this->m_ptr=new int[this->m_size];
+			for (int i = 0; i< obj.m_size;i++)
+			{
+				this->m_ptr[i] = obj.m_ptr[i];
+			}
+			obj.m_ptr=nullptr;
+			obj.m_size=0;
+			obj.m_weight=0;
+			obj.m_sum_of_weights=0;
+			std::cout<<"ANIMAL move constructor"<<std::endl;
+		}
+		ANIMAL&& operator=(ANIMAL&& obj)
+		{
+			if (this==&obj)
+			{
+				return  std::move(*this);
+			}
+			delete []this->m_ptr;
+			this->m_ptr=obj.m_ptr;
+			this->m_size=obj.m_size;
+			for (int i = 0; i< obj.m_size;i++)
+			{
+				this->m_ptr[i] = obj.m_ptr[i];
+			}
+			obj.m_ptr=nullptr;
+			obj.m_size=0;
+			this->m_weight=obj.m_weight;
+			obj.m_weight=0;
+			this->m_sum_of_weights=obj.m_sum_of_weights;
+			obj.m_sum_of_weights=0;
+			std::cout<<"ANIMAL move operator assignment"<<std::endl;
+			return std::move(*this);
+		}
+		~ANIMAL()
+		{
+			delete []m_ptr;
+			std::cout<<"ANIMAL destructor"<<std::endl;
+		}
+	friend bool operator> (const ANIMAL& a1, const ANIMAL& a2)
+	{
+		return a1.m_weight > a2.m_weight;
+	}
+
+	friend bool operator< (const ANIMAL& a1, const ANIMAL& a2)
+	{
+		return a1.m_weight < a2.m_weight;
+	}
+	
+    int operator()(int animal_weight) {
+      return m_sum_of_weights + animal_weight;
+    }
+};
+
+int main ()
+{
+	
+	ANIMAL pig(60,10);
+	std::cout<<"step 1"<<std::endl;
+	ANIMAL pig1 = std::move(pig);
+	std::cout<<"step 2"<<std::endl;
+	ANIMAL horse(120,20);
+	std::cout<<"step 3"<<std::endl;
+	ANIMAL horse1(110,20);
+	std::cout<<"step4"<<std::endl;
+	horse1=std::move(horse);
+	std::cout<<"step5"<<std::endl;
+	return 0;
+}
---
+++ move_ctr_oper/matrix.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <stdlib.h>
+int matrix_size_is()
+{
+	int size_of_matrix;
+	do 
+	{
+		std::cout<<"Enter size of matrix: ";
+		std::cin>>size_of_matrix;
+		
+	}
+	while (size_of_matrix<4);
+	
+	return size_of_matrix;
+}
+class MATRIX
+{
+	private:
+		int size_of_matrix;
+		int** ptr;
+	public:
+			
+	
+	MATRIX();
+	MATRIX(int size)
+	{
+		if (size<4)
+		{
+			size_of_matrix=matrix_size_is();
+		} else
+		{
+			size_of_matrix=size;
+		}
+		
+		ptr = new int*[size_of_matrix];
+		for (int i = 0; i< size_of_matrix;i++)
+		{
+			ptr[i] = new int[size_of_matrix];
+		}
+		for(int i=0; i<size_of_matrix;i++)
+		{
+			for(int j=0; j<size_of_matrix;j++)
+			{
+				ptr[i][j]=rand()%100;
+			}
+		}
+		std::cout<<"MATRIX constructor"<<std::endl;
+	}
+	MATRIX(MATRIX &obj)
+	{
+		this->size_of_matrix=obj.size_of_matrix;
+		this->ptr=new int*[this->size_of_matrix];
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			this->ptr[i] = new int[obj.size_of_matrix];
+		}
+		for(int i=0; i<obj.size_of_matrix;i++)
+		{
+			for(int j=0; j<obj.size_of_matrix;j++)
+			{
+				this->ptr[i][j]=obj.ptr[i][j];
+			}
+		}
+		std::cout<<"MATRIX copy constructor"<<std::endl;
+		
+	}
+	MATRIX& operator=(MATRIX& obj)
+	{
+		if (this==&obj)
+		{
+			return *this;
+		}
+		for (int i = 0; i< size_of_matrix;i++)
+		{
+			delete []ptr[i];
+		}
+		delete []ptr;
+		this->size_of_matrix=obj.size_of_matrix;
+		this->ptr=new int*[this->size_of_matrix];
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			this->ptr[i] = new int[obj.size_of_matrix];
+		}
+		for(int i=0; i<obj.size_of_matrix;i++)
+		{
+			for(int j=0; j<obj.size_of_matrix;j++)
+			{
+				this->ptr[i][j]=obj.ptr[i][j];
+			}
+		}
+		std::cout<<"operator assignment"<<std::endl;
+		return *this;
+	}
+	MATRIX(MATRIX&& obj)
+	{
+		this->size_of_matrix=obj.size_of_matrix;
+		this->ptr=new int*[this->size_of_matrix];
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			this->ptr[i] = new int[obj.size_of_matrix];
+		}
+		for(int i=0; i<obj.size_of_matrix;i++)
+		{
+			for(int j=0; j<obj.size_of_matrix;j++)
+			{
+				this->ptr[i][j]=obj.ptr[i][j];
+			}
+		}
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			obj.ptr[i] = nullptr;
+		}
+		obj.ptr= nullptr;
+		obj.size_of_matrix =0;
+		std::cout<<"MATRIX move constructor"<<std::endl;
+		
+	}
+	MATRIX&& operator=(MATRIX&& obj)
+	{
+		if (this==&obj)
+		{
+			return std::move(*this);
+		}
+		for (int i = 0; i< this->size_of_matrix;i++)
+		{
+			delete []ptr[i];
+		}
+		delete []ptr;
+		this->size_of_matrix=obj.size_of_matrix;
+		this->ptr=new int*[this->size_of_matrix];
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			this->ptr[i] = new int[obj.size_of_matrix];
+		}
+		for(int i=0; i<obj.size_of_matrix;i++)
+		{
+			for(int j=0; j<obj.size_of_matrix;j++)
+			{
+				this->ptr[i][j]=obj.ptr[i][j];
+			}
+		}
+		for (int i = 0; i< obj.size_of_matrix;i++)
+		{
+			obj.ptr[i] = nullptr;
+		}
+		obj.ptr= nullptr;
+		obj.size_of_matrix =0;
+		std::cout<<"MATRIX move operator assignment"<<std::endl;
+		return std::move(*this);
+	}
+	~MATRIX()
+	{
+		for (int i = 0; i< size_of_matrix;i++)
+		{
+			delete []ptr[i];
+		}
+		delete []ptr;
+		std::cout<<"MATRIX destructor"<<std::endl;
+	}
+	void print_matrix()
+	{
+		for (int i=0; i<size_of_matrix;i++)
+		{
+			for (int j=0;j<size_of_matrix;j++)
+			{
+				std::cout<<ptr[i][j]<<" ";
+			}
+			std::cout<<std::endl;
+		}
+		std::cout<<std::endl;
+	}
+	void transfer_columns(int col_first, int col_second)
+	{
+		for (int i=0; i<size_of_matrix; i++)
+		{
+			ptr[i][col_first]=ptr[i][col_first]+ptr[i][col_second];
+			ptr[i][col_second]=ptr[i][col_first]-ptr[i][col_second];
+			ptr[i][col_first]=ptr[i][col_first]-ptr[i][col_second];
+		}
+	}
+
+	void transfer(int first, int second, std::string col_row)
+	{
+		if ((first > size_of_matrix) ||  (second>size_of_matrix))
+		{
+			std::cout<<"Wrong rows entered";
+		} 
+		else
+		{
+			if (col_row.compare("row") == 0) 
+			{
+				for (int i=0; i<size_of_matrix; i++)
+				{
+					ptr[first][i]=ptr[first][i]+ptr[second][i];
+					ptr[second][i]=ptr[first][i]-ptr[second][i];
+					ptr[first][i]=ptr[first][i]-ptr[second][i];
+				}
+			} else if (col_row.compare("col") == 0)
+			{
+				for (int i=0; i<size_of_matrix; i++)
+				{
+					ptr[i][first]=ptr[i][first]+ptr[i][second];
+					ptr[i][second]=ptr[i][first]-ptr[i][second];
+					ptr[i][first]=ptr[i][first]-ptr[i][second];
+				}
+			} else
+			{
+				std::cout<<"Please enter 'row' or 'col'"<<std::endl;
+			}
+		}			
+	}
+};
+
+int main()
+{
+	int size_of_matrix;
+	size_of_matrix=matrix_size_is();
+	MATRIX obj(size_of_matrix);
+	std::cout<<"step 1"<<std::endl;
+	MATRIX obj2 = std::move(obj);
+	std::cout<<"step 2"<<std::endl;
+	MATRIX obj3(size_of_matrix);
+	std::cout<<"step 3"<<std::endl;
+	MATRIX obj4(size_of_matrix);
+	std::cout<<"step 4"<<std::endl;
+	obj4=std::move(obj3);
+	std::cout<<"step 5"<<std::endl;
+	return 0;
+}
