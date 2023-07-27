84781db09fe2e2b4d65ce95f71c5d54dd2d607c1
ArthurNigoyan <arthur.goyan@gmail.com>
2023-07-27 23:24:46 +0400

    template example for fibonacci

--- templates/fibonacci/fibonacci.cpp
+++ templates/fibonacci/fibonacci.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <vector>
-template <typename Z>
-Z fibonacci(Z n)
-{
-	if (n==0) {
-		return 0;
-	} else if (n==1) 
-	{
-		return 1;
-	}		
-	return fibonacci(n-1)+fibonacci(n-2);
-	
-}
 
+
+template <unsigned int n>
+struct fibonacci
+{
+			
+	enum { value =fibonacci<n-1>::value+fibonacci<n-2>::value };
+	
+};
+
+template <>
+struct fibonacci<0>
+{
+			
+	enum { value = 0 };
+	
+};
+template <>
+struct fibonacci<1>
+{
+			
+	enum { value = 1 };
+	
+};
 
 
 int main()
 {
-	// std::cout<<factorial(3)<<std::endl;;
-	// std::cout<<factorial("b");
-	 std::cout<<fibonacci(15)<<std::endl;
-	 std::cout<<fibonacci(10)<<std::endl;
-	 // fibonacci<40>::value;fibonacci
-	 std::vector<int> altern;
-	 for (int i = 0; i <= 10; i++)
-        altern.push_back(fibonacci(i));
-	 for (auto i = altern.begin(); i != altern.end(); ++i)
-        std::cout<<*i <<" ";
+	 std::cout<<fibonacci<15>::value<<std::endl;
+	 std::cout<<fibonacci<10>::value<<std::endl;
+	 std::cout<<fibonacci<0>::value<<std::endl;
+	 std::cout<<fibonacci<1>::value<<std::endl;
+	
+	
 	return 0;
 }
